21434c240abadf5fb4148c104a9cedf10ae061f5
iqbal <iqbalagil@gmai.com>
2024-07-11 07:44:23 +0700

    feat: add animation and trigger

--- Assets/Script/Fruit/FruitCompleted.cs
+++ Assets/Script/Fruit/FruitCompleted.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-
 public class FruitCompleted : MonoBehaviour
 {
     [SerializeField] private List<FruitHandler> _slotPrefabs;
     [SerializeField] private List<FruitSelector> _slotPiece;
-    [SerializeField] private FruitSelector _fruitPiece;
     [SerializeField] private Transform _slotParent, _pieceParent;
 
     void Start()
@@ -16,14 +14,20 @@ public class FruitCompleted : MonoBehaviour
         Spawn();
     }
 
-    void Spawn() {
-        var randomSpawn = _slotPrefabs.OrderBy(s => Random.value).Take(3).ToList(); 
+    void Spawn()
+    {
+        var indices = Enumerable.Range(0, _slotPrefabs.Count).OrderBy(i => Random.value).ToList();
 
-        for (int i = 0; i < randomSpawn.Count; i++)
+        var selectedIndices = indices.Take(3).ToList();
+
+        for (int i = 0; i < selectedIndices.Count; i++)
         {
-            var spawnSlot = Instantiate(randomSpawn[i], _slotParent.GetChild(i).position, Quaternion.identity);
+            int index = selectedIndices[i];
 
-            var spawnPiece = Instantiate(_fruitPiece, _pieceParent.GetChild(i).position, Quaternion.identity);
+            var spawnSlot = Instantiate(_slotPrefabs[index], _slotParent.GetChild(i).position, Quaternion.identity, _slotParent.GetChild(i));
+
+            var spawnPiece = Instantiate(_slotPiece[index], _pieceParent.GetChild(i).position, Quaternion.identity, _pieceParent.GetChild(i));
+
             spawnPiece.Init(spawnSlot);
         }
     }
--- Assets/Script/Fruit/FruitSelector.cs
+++ Assets/Script/Fruit/FruitSelector.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class FruitSelector : MonoBehaviour
 {
-    [SerializeField] SpriteRenderer _renderer;
-    private bool _dragging, _placed;
+    [SerializeField] private Animator fruitAnimation;
+    [SerializeField] private GameObject prefabsObject;
+    [SerializeField] private SpriteRenderer _renderer;
+    public bool _dragging, _placed;
     private Vector2 offset, _originalPosition;
-    private FruitHandler _slot;
-    private GameObject _piece;
+    public FruitHandler _slot;
+    private Vector3 originalScale;
+    public FruitSelector _selector;
 
     public void Init(FruitHandler slot)
     {
-        _renderer.sprite = slot.Renderer.sprite;
         _slot = slot;
     }
 
@@ -28,34 +30,109 @@ public class FruitSelector : MonoBehaviour
         if (!_dragging) return;
 
         var mousePosition = GetMousePos();
-
         transform.position = mousePosition - offset;
+
+
+        if (_selector._renderer.enabled == false)
+        {
+            Debug.Log("Renderer was disabled, now enabling it.");
+            _selector._renderer.enabled = true;
+            fruitAnimation.SetBool("HappyRabbit", true);
+        }
+        else if (_selector._renderer.enabled == true)
+        {
+            if (fruitAnimation.GetBool("HappyRabbit") == true)
+            {
+                StartCoroutine(HandleFruitSpawn());
+            }
+            else
+            {
+                fruitAnimation.SetBool("SadRabbit", true);
+            }
+        }
 
     }
 
     private void OnMouseDown()
     {
-        _dragging = true;
+        if (_placed) return;
 
+        _dragging = true;
         offset = GetMousePos() - (Vector2)transform.position;
     }
 
     private void OnMouseUp()
     {
-        if(Vector2.Distance(transform.position, _slot.transform.position) < 3){
-            transform.position = -_slot.transform.position;
+        _dragging = false;
+
+        if (Vector2.Distance(transform.position, _slot.transform.position) < 3)
+        {
+            transform.position = _slot.transform.position;
             _placed = true;
-        } else
-            {
-            transform.position = _originalPosition;
-            _dragging = false;
+            StartCoroutine(DisappearAfterSnap());
         }
-         
-        
+        else
+        {
+            transform.position = _originalPosition;
+        }
+    }
+
+    private IEnumerator DisappearAfterSnap()
+    {
+        yield return new WaitForSeconds(0f);
+
+        _renderer.enabled = false;
+
+        if (_slot != null && _slot.Renderer != null)
+        {
+            _slot.Renderer.enabled = false;
+        }
+
+        if (_slot != null)
+        {
+            _slot.gameObject.SetActive(false);
+        } 
+
+        //_slot.gameObject.SetActive(false);
     }
 
     Vector2 GetMousePos()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+
+    //private AppearAfterSnap()
+    //{
+
+    //}
+
+    private IEnumerator HandleFruitSpawn()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        float scaleDuration = 1f;
+        float time = 0;
+        Vector3 targetScale = Vector3.zero;
+
+        if (_slot.gameObject.activeSelf == false)
+        {
+            _slot.gameObject.SetActive(true);
+
+            while (time < scaleDuration)
+            {
+                _slot.gameObject.transform.localScale = Vector3.Lerp(originalScale, targetScale, time / scaleDuration);
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            _slot.gameObject.transform.localScale = targetScale;
+
+            Animator spawnedFruitAnimator = _slot.GetComponent<Animator>();
+            if (spawnedFruitAnimator != null)
+            {
+                spawnedFruitAnimator.SetTrigger("ElevationAnimation");
+            }
+        }
+    }
+
 }
